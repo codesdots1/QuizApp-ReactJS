814ea2d62bdf2494a63b4c3370e5db454405d4b4
Codes Dots Solutions <codesdot07@gmail.com>
2022-12-30 12:08:52 +0530

    initial Commit
    initial Commit

---
+++ src/helpers/Questions.js
@@ -0,0 +1,34 @@
+export const Questions = [
+    {
+        prompt: "What is my name?",
+        optionA: "Harsh Rana",
+        optionB: "Piyush Soni",
+        optionC: "Jay Kanani",
+        optionD: "Harsh Lad",
+        asnwer: "optionD",
+    },
+    {
+        prompt: "Which of this is not a programming language?",
+        optionA: "Python",
+        optionB: "JavaScript",
+        optionC: "MC-03",
+        optionD: "Java",
+        asnwer: "optionC",
+    },
+    {
+        prompt: "Which of this is not a javascript framework?",
+        optionA: "React",
+        optionB: "Angular",
+        optionC: "Vue",
+        optionD: "Java",
+        asnwer: "optionD",
+    },
+    {
+        prompt: "Which programming language are you working on?",
+        optionA: "Wordpress",
+        optionB: "React",
+        optionC: "PHP",
+        optionD: ".Net",
+        asnwer: "optionC",
+    },
+];
